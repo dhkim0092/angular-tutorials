b3f3dec84d8d1af94551b5c6dbff0b4de65aab4b
dhkim0092 <dhkim0092@naver.com>
2020-02-04 23:02:09 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-yk5aus-i9hnps
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-yk5aus-i9hnps)
---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root', //응용프로그램 셸, 가장 먼저 로드되는 구성요소, 다른 모든구성 요소의 부모.
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {}
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
